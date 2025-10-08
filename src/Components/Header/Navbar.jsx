ecd9018e0d4f7e5b65d0cb2ed54813fa0ece8e43
Resalat Al Samin <risalat731@gmail.com>
2025-10-08 13:56:23 +0600

    navbar

---
+++ src/Components/Header/Navbar.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import logoImg from "../../assets/assets/logo.png";
+import btnIcon from "../../assets/assets/icons8.png";
+
+const NavBar = () => {
+  return (
+    <div className="navbar bg-white shadow-sm lg:max-w-7xl lg:mx-auto font-display">
+      <div className="navbar-start">
+        <div className="dropdown text-black">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow text-black font-medium"
+          >
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Apps</a>
+            </li>
+            <li>
+              <a>Installation</a>
+            </li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">
+          <img className="w-10 " src={logoImg} alt="" />
+          <h1 className="bg-gradient-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent ">
+            HERO.IO
+          </h1>
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 text-black font-medium">
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>Apps</a>
+          </li>
+          <li>
+            <a>Installation</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end ">
+        <a className="btn bg-gradient-to-r from-[#632EE3] to-[#9F62F2] lg:w-[145px] lg:h-[43px] border-none ">
+          <img src={btnIcon} alt="" />
+          <h2 className="text-white font-semibold">Contribute</h2>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from "../Root/Root";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import Home from "../Pages/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        index: true,
+        path: "/",
+        Component: Home,
+      },
+    ],
+  },
+]);
